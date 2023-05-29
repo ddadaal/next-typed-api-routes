19efcc2e49bdf6373a24fb7e8f6cecd7bc54c517
Chen Junda <ddadaal@outlook.com>
2023-05-29 17:17:10 +0800

    chore: add playwright test

--- example/tests/apis.test.ts
+++ example/tests/apis.test.ts
@@ -1,6 +1,7 @@
 import { api } from "../src/apis/api";
+import { test, expect } from '@playwright/test';
 
-it("should call zodRoute", async () => {
+test("should call zodRoute", async ({ page }) => {
   const resp = await api.zodRoute({ body: { info: "test" }, query: { test: "123" } });
 
   expect(resp.hello).toBe("test 123");
